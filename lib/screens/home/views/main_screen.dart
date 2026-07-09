dee67d57ca02f976defea556de15d7e3f73c459a
codemantechnology <codemantech630@gmail.com>
2026-07-09 12:25:17 +0300

    comments

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -204,6 +204,7 @@ class MainScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 40),
+            //Transactions
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
